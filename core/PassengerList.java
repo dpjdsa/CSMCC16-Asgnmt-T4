b36abe39cc19631df89c1aa4ce7bc366a63fad7a
dpjdsa <71557388+dpjdsa@users.noreply.github.com>
2021-01-22 10:03:20 +0000

    Update PassengerList.java

--- core/PassengerList.java
+++ core/PassengerList.java
@@ -2,7 +2,7 @@ package core;
 import java.util.List;
 import java.util.ArrayList;
 /**
- * Class used to store list of passengers
+ * Class used to store list of passenger records
  * @author BD837672
  * @version 20th December 2020
  */
@@ -15,14 +15,17 @@ public class PassengerList {
     {
         pList = new ArrayList<>();
     }
+    // Add passenger to passenger record list
     public void addPassenger(Passenger passengerIn)
     {
         pList.add(passengerIn);
     }
+    // Get size of passenger record list
     public int size()
     {
         return pList.size();
     }
+    // Get passenger at position positionIn
     public Passenger getPassenger(int positionIn){
         return pList.get(positionIn);
     }
