0adf293b6246e4b37133b6a475fe4cad589d8d6e
dpjdsa <71557388+dpjdsa@users.noreply.github.com>
2021-01-22 10:05:17 +0000

    Update ReadAndErrorCheck.java

--- core/ReadAndErrorCheck.java
+++ core/ReadAndErrorCheck.java
@@ -12,10 +12,10 @@ import java.util.HashMap;
  * Reading, Error Checking and Correcting input file
  * Reads in Airport list and Passenger record file.
  * Assumes airport list is error free but corrects passenger record fields
- * which have incorrect formats based on finding lowest Levensthein
+ * which have incorrect formats based on finding lowest Levenshtein
  * score to fields that have already been scanned correctly.
- * 
- *
+ * @author BD837672
+ * @version 22nd January 2021
  */
 public class ReadAndErrorCheck
     {
@@ -139,7 +139,7 @@ public class ReadAndErrorCheck
                                     fltCodeList.replace(fltCodeKey,(Integer)(fltCodeList.get(fltCodeKey))+1);    
                                 } else {
                                     // Otherwise if new, check against all existing flight codes used more than once 
-                                    //to see if there are nay which are a Levensthein distance of only 1 away.
+                                    //to see if there are any which are a Levenshtein distance of only 1 away.
                                     for(Object currentFltCode : fltCodeList.keySet()){
                                         if((Levensthein(currentFltCode.toString(),fltCodeKey)<2)&&((Integer)fltCodeList.get(currentFltCode)>1)){
                                             System.out.print("\n** Incorrect Closely Matching Flight Code: "+FltId);
@@ -190,7 +190,7 @@ public class ReadAndErrorCheck
             e.printStackTrace();
         }
     }
-    // Calculates Levensthein distance between two strings
+    // Calculates Levenshtein distance between two strings
     // From https://en.wikipedia.org/wiki/Levenshtein_distance accessed on 6/1/21 at 12am.
     public static int Levensthein(String S1,String S2){
         int m=S1.length();
