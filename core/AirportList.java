4665fc7a3959798b9962e7f387a3c0bb103dba3f
dpjdsa <71557388+dpjdsa@users.noreply.github.com>
2021-01-22 09:42:50 +0000

    Update AirportList.java

--- core/AirportList.java
+++ core/AirportList.java
@@ -18,14 +18,17 @@ public class AirportList {
         apList=new HashMap<>();
         MAX=MaxIn;
     }
+    // Adds airport to airport list
     public void addAirport(Airport airportIn)
     {
         apList.put(airportIn.getCode(),airportIn);
     }
+    // Returns size of airport list
     public int size()
     {
         return apList.size();
     }
+    // Methods to get airport values based on airport code
     public String getName(String airportCodeIn)
     {
         return apList.get(airportCodeIn).getName();
@@ -38,10 +41,12 @@ public class AirportList {
     {
         return apList.get(airportCodeIn).getLon();
     }
+    // Get the set of airport codes
     public Set<String> getKeys()
     {
         return apList.keySet();
     }
+    // Check that airport code is valid
     public boolean validCode(String codeIn)
     {
         if (apList.containsKey(codeIn)){
@@ -50,6 +55,7 @@ public class AirportList {
             return false;
         }
     }
+    //Get airport codes as a HashSet
     public HashSet<String> getKeysHashSet()
     {
         HashSet<String> set=new HashSet<String>();
